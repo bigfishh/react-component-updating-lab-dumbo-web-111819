c1d0e37e5715dc05d09893e8809630c4697d5b2e
bigfishh <anniezheng.anniee@gmail.com>
2020-02-01 17:03:06 -0500

    hello

--- src/Timer.js
+++ src/Timer.js
@@ -15,7 +15,7 @@ class Timer extends Component {
     this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate(nextState) {
     if (this.state.time === nextState.time) {
       return false
     }
